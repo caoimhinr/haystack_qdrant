d3ca1caa65e367417c79e1c0e608010dfe0a63a8
caoimhinr <kevin@welvaert.org>
2025-10-02 21:36:21 +0200

    change pipeline run

--- qdrant_indexing/pipelines/qdrant_indexing/pipeline_wrapper.py
+++ qdrant_indexing/pipelines/qdrant_indexing/pipeline_wrapper.py
@@ -8,42 +8,51 @@ from haystack.components.embedders import SentenceTransformersDocumentEmbedder
 from haystack.components.writers import DocumentWriter
 from haystack_integrations.document_stores.qdrant import QdrantDocumentStore
 
-
 class PipelineWrapper(BasePipelineWrapper):
     def setup(self) -> None:
-        indexing = Pipeline()
-        indexing.add_component("converter", TextFileToDocument())
-        indexing.add_component("embedder", SentenceTransformersDocumentEmbedder())
-        document_store = QdrantDocumentStore(host="qdrant")
-        indexing.add_component("writer", DocumentWriter(document_store=document_store))
-        indexing.connect("converter", "embedder")
-        indexing.connect("embedder", "writer")
+        """Setup a pipeline with a default Qdrant collection"""
+        # Create a reusable document store
+        self.document_store = QdrantDocumentStore(
+            host="qdrant",
+            index="default",
+            recreate_index=False,  # Do not overwrite existing collection
+            embedding_dim=768
+        )
 
-        self.pipeline = indexing
+        # Create a single pipeline
+        self.pipeline = Pipeline()
+        self.pipeline.add_component("converter", TextFileToDocument())
+        self.pipeline.add_component("embedder", SentenceTransformersDocumentEmbedder())
+        self.pipeline.add_component("writer", DocumentWriter(document_store=self.document_store))
+        self.pipeline.connect("converter", "embedder")
+        self.pipeline.connect("embedder", "writer")
 
     def run_api(self, files: Optional[List[UploadFile]] = None, collection_name: str = "default") -> dict:
-        if files:
-            # Replace the writer with a new one pointing to the right collection
-            document_store = QdrantDocumentStore(host="qdrant", 
-            index=collection_name,
-            recreate_index=False)
-            
-            indexing = Pipeline()
-            indexing.add_component("converter", TextFileToDocument())
-            indexing.add_component("embedder", SentenceTransformersDocumentEmbedder())
-            indexing.add_component("writer", DocumentWriter(document_store=document_store))
-            indexing.connect("converter", "embedder")
-            indexing.connect("embedder", "writer")
-
-            for file in files:
-                text = file.file.read().decode("utf-8")
-                log.debug(f"Indexing file: {file.filename} into collection {collection_name}")
-
-                indexing.run(
-                    {"converter": {"sources": [ByteStream(text.encode())]}}
-                )
-        else:
+        if not files:
             log.debug("No files to index")
+            return {"success": True}
+
+        # Update document store to use the requested collection
+        if self.document_store.index != collection_name:
+            self.document_store = QdrantDocumentStore(
+                host="qdrant",
+                index=collection_name,
+                recreate_index=False,  # Do not overwrite existing data
+                embedding_dim=768
+            )
+            # Update writer in the pipeline to point to the new collection
+            self.pipeline.remove_component("writer")
+            self.pipeline.add_component("writer", DocumentWriter(document_store=self.document_store))
+            self.pipeline.connect("embedder", "writer")
+
+        # Prepare all files for a single pipeline run
+        sources = []
+        for file in files:
+            text = file.file.read().decode("utf-8")
+            sources.append(ByteStream(text.encode()))
+            log.debug(f"Prepared file: {file.filename} for indexing")
+
+        # Run pipeline once for all files
+        self.pipeline.run({"converter": {"sources": sources}})
 
         return {"success": True}
-
